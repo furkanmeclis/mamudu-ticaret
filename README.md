2a5cd1a5d8d492e3d0d4418ce88942749fe76114
furkanmeclis <71632315+furkanmeclis@users.noreply.github.com>
2023-02-12 21:09:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mamudu-ticaret
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-lymxvz)
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import { ChakraProvider } from '@chakra-ui/react';
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
