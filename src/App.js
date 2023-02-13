293ca3615dbc7c2fb9985f8a94155c3e02caf5da
furkanmeclis <71632315+furkanmeclis@users.noreply.github.com>
2023-02-13 21:52:00 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import { Stack, Box, VStack } from '@chakra-ui/react';
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Box w="full" display="flex" h="full" alignItems="center" justify="center">
+      <Box w="40px" h="full" bg="yellow.200">
+        1
+      </Box>
+      <Box w="40px" h="40px" bg="tomato">
+        2
+      </Box>
+    </Box>
   );
 }
